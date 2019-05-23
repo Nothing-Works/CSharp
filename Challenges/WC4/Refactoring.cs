7e9274ddb92095ce859065530d52494cfc586f8d
AndySong <andylauszp@gmail.com>
2019-05-24 11:53:40 +1200

    add wc4

---
+++ Challenges/WC4/Refactoring.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenges.WC4
+{
+    public class Refactoring
+    {
+        public void Done()
+        {
+            //This one is for refactoring, which I am not a big fan of so I will just skip it.
+        }
+    }
+}
