71be9044d5c84a5d89490a3864c6f36649503fdf
AndySong <andylauszp@gmail.com>
2019-06-18 14:03:57 +1200

    delegate sample

---
+++ Challenges/CSharpIND3/CSharp1/DelegateExample/DelegateSample.cs
@@ -0,0 +1,25 @@
+﻿namespace Challenges.CSharpIND3.CSharp1.DelegateExample
+{
+    class DelegateSample
+    {
+        //1. declare delegate type
+        delegate string GiveMeTheString(string s);
+
+        //2. create a method
+        string DoesNothing(string s)
+        {
+            return s;
+        }
+
+        void RunIt()
+        {
+            //3. create delegate instance
+            GiveMeTheString del1;
+
+            del1 = new GiveMeTheString(DoesNothing);
+
+            //4. invoke the instance
+            del1("Andy");
+        }
+    }
+}
