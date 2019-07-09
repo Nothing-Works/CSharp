8abcc6561e7e42cb8372749bd5e59e0201aba9f4
AndySong <andylauszp@gmail.com>
2019-07-10 11:16:58 +1200

    auto properties

---
+++ Challenges/CSharpIND3/CSharp3/AutomaticProperties.cs
@@ -0,0 +1,43 @@
+﻿namespace Challenges.CSharpIND3.CSharp3
+{
+    public class AutomaticProperties
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        private static int InstanceCounter { get; set; }
+
+        private static readonly object counterLock = new object();
+
+        public void InstanceCountingAutomaticProperties(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+            lock (counterLock)
+            {
+                InstanceCounter++;
+            }
+        }
+    }
+
+    public struct Foo
+    {
+        public int Value { get; private set; }
+
+        public Foo(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public struct Bar
+    {
+        public int Value { get; private set; }
+
+        public Bar(int value) : this()
+        {
+            this.Value = value;
+        }
+    }
+}
