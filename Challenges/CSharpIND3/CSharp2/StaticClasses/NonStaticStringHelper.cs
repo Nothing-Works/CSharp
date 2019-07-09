85d301c1bddfd0531671dfb489b28904de463973
AndySong <andylauszp@gmail.com>
2019-07-09 15:04:24 +1200

    static class

---
+++ Challenges/CSharpIND3/CSharp2/StaticClasses/NonStaticStringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Challenges.CSharpIND3.CSharp2.StaticClasses
+{
+    public sealed class NonStaticStringHelper
+    {
+        private NonStaticStringHelper()
+        {
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
---
+++ Challenges/CSharpIND3/CSharp2/StaticClasses/StringHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Challenges.CSharpIND3.CSharp2.StaticClasses
+{
+    public static class StringHelper
+    {
+        public static string Reverse(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
