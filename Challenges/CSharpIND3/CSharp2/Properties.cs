e289fa5e63bc0917307f6541a7f16a5e3c2b314e
AndySong <andylauszp@gmail.com>
2019-07-09 15:14:12 +1200

    properties

---
+++ Challenges/CSharpIND3/CSharp2/Properties.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenges.CSharpIND3.CSharp2
+{
+    public class Properties
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+    }
+}
