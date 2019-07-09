35bd096d3efa29398a51e6964e39e9e5fb0c212c
AndySong <andylauszp@gmail.com>
2019-07-10 11:35:29 +1200

    auto propreties CSharp2

---
+++ Challenges/CSharpIND4/CSharp3/AutoProperties.cs
@@ -0,0 +1,20 @@
+﻿namespace Challenges.CSharpIND4.CSharp3
+{
+    public class AutoProperties
+    {
+        //C# 1 and 2.
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        //C# 3 or higher.
+        public string NameAuto { get; set; }
+
+        public string NamePrivate { get; private set; }
+    }
+}
