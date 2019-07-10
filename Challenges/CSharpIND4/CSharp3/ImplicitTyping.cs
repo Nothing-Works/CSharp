38ee74f894aa551c4f3e4d2681650c23ec9122a0
AndySong <andylauszp@gmail.com>
2019-07-10 14:07:49 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp3/ImplicitTyping.cs
@@ -0,0 +1,14 @@
+﻿namespace Challenges.CSharpIND4.CSharp3
+{
+    public class ImplicitTyping
+    {
+        int[] array = new int[10];
+
+        // Before C# 3.
+        int[] array1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+        //After C# 3.
+        int[] array3 = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    }
+}
