b7317dbdddcc74c5f3827a7f15fce62a2b47eee5
AndySong <andylauszp@gmail.com>
2019-07-11 17:32:42 +1200

    wip

---
+++ Challenges/CSharpIND3/CSharp3/Lambda/Example.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Challenges.CSharpIND3.CSharp3.Lambda
+{
+    public class Example
+    {
+        public void Test()
+        {
+            Func<string, int> returnLength;
+
+            returnLength = delegate(string s) { return s.Length; };
+
+            Console.WriteLine(returnLength("Andy"));
+
+
+            Func<string, int> returnLengthL;
+
+            returnLengthL = (string s) => { return s.Length; };
+
+            Console.WriteLine(returnLengthL("Andy"));
+
+
+            Func<string, int> returnLengthL1;
+
+            returnLengthL1 = (string s) => s.Length;
+
+            Console.WriteLine(returnLengthL1("Andy"));
+
+            Func<string, int> returnLengthL2;
+
+            returnLengthL2 = (s) => s.Length;
+
+            Console.WriteLine(returnLengthL2("Andy"));
+
+            Func<string, int> returnLengthL3;
+
+            returnLengthL3 = s => s.Length;
+
+            Console.WriteLine(returnLengthL3("Andy"));
+        }
+    }
+}
---
+++ Challenges/CSharpIND3/CSharp3/Lambda/Film.cs
@@ -0,0 +1,8 @@
+﻿namespace Challenges.CSharpIND3.CSharp3.Lambda
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
