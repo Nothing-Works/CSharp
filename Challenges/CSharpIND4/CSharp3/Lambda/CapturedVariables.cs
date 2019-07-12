0b7ec58d29dca94d13a68de2022a8e4a16a829fe
AndySong <andylauszp@gmail.com>
2019-07-12 12:33:43 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp3/Lambda/CapturedVariables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp3.Lambda
+{
+    public class CapturedVariables
+    {
+        private string instanceField = "instance field";
+
+        public Action<string> CreateAction(string methodParameter)
+        {
+            string methodLocal = "method local";
+            string uncaptured = "uncaptured local";
+
+            Action<string> action = lamdaParemeter =>
+            {
+                string lambdaLocal = "lambda local";
+                Console.WriteLine("Instance field : {0}", instanceField);
+                Console.WriteLine("Method parameter : {0}", methodParameter);
+                Console.WriteLine("Method local : {0}", methodLocal);
+                Console.WriteLine("lambda parameter : {0}", lamdaParemeter);
+                Console.WriteLine("Lambda Local : {0}", lambdaLocal);
+            };
+
+            return action;
+        }
+    }
+}
---
+++ Challenges/CSharpIND4/CSharp3/Lambda/Example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp3.Lambda
+{
+    public class Example
+    {
+        Action<string> action = delegate(string s) { Console.WriteLine(s); };
+
+        Action<string> action1 = s => Console.WriteLine(s);
+
+        Func<int, int, int> multiply = (a, b) => a * b;
+
+        Func<string, int> squareLength = s => s.Length * s.Length;
+
+        public void Test()
+        {
+            action("Hahahah");
+        }
+    }
+}
