24e8617c7c7858cedefe1eba52b3316d0476a4d2
AndySong <andylauszp@gmail.com>
2019-07-23 11:14:22 +1200

    expressiontree 4

---
+++ Challenges/CSharpIND4/CSharp3/ExpressionTree/Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Challenges.CSharpIND4.CSharp3.ExpressionTree
+{
+    public class Example
+    {
+        Expression<Func<int, int, int>> adder = (x, y) => x + y;
+
+        public void Go()
+        {
+            Console.WriteLine(adder);
+            Func<int, int, int> compiled = adder.Compile();
+            Console.WriteLine(compiled(2, 3));
+            Console.WriteLine(compiled);
+        }
+    }
+}
