14e5eb9fc3a9ff231341fc9928e2654fa4b462c4
AndySong <andylauszp@gmail.com>
2019-07-24 14:53:25 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp3/ExtensionMethods/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenges.CSharpIND4.CSharp3.ExtensionMethods
+{
+    public class Example
+    {
+        string[] words = { "keys", "coat", "laptop", "bottle" };
+
+        public void Test()
+        {
+            IEnumerable<string> query = words.Where(c => c.Length > 4)
+                .OrderBy(c => c)
+                .Select(c => c.ToUpper());
+
+            foreach (string s in query)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
