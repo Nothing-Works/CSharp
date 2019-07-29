fd2eadd3841246227227289b1902b8c5c83136b6
AndySong <andylauszp@gmail.com>
2019-07-29 16:13:29 +1200

    wip

--- Challenges/CSharpIND3/CSharp3/LinqToObjectExtension/AllMethods.cs
+++ Challenges/CSharpIND3/CSharp3/LinqToObjectExtension/AllMethods.cs
@@ -1,6 +1,61 @@
-﻿namespace Challenges.CSharpIND3.CSharp3.LinqToObjectExtension
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenges.CSharpIND3.CSharp3.LinqToObjectExtension
 {
-    public class AllMethods
+    public static class AllMethods
     {
+        public static T RandomElement<T>(this IEnumerable<T> source, Random random)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException("source");
+            }
+
+            if (random == null)
+            {
+                throw new ArgumentException("random");
+            }
+
+            ICollection collection = source as ICollection;
+
+            if (collection != null)
+            {
+                int count = collection.Count;
+
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("Sequence was empty");
+                }
+
+                int index = random.Next(count);
+                return source.ElementAt(index);
+            }
+
+            using (IEnumerator<T> iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    throw new InvalidOperationException("Sequence was empty");
+                }
+
+                int countSoFar = 1;
+                T current = iterator.Current;
+
+                while (iterator.MoveNext())
+                {
+                    countSoFar++;
+
+                    if (random.Next(countSoFar) == 0)
+                    {
+                        current = iterator.Current;
+                    }
+                }
+
+                return current;
+            }
+        }
     }
 }
