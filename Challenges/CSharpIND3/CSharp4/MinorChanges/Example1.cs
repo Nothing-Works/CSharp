6e1b45237b83057523c3faa0e6f007f113a14897
AndySong <andylauszp@gmail.com>
2019-07-30 15:11:25 +1200

    named

--- Challenges/CSharpIND3/CSharp4/MinorChanges/Example1.cs
+++ Challenges/CSharpIND3/CSharp4/MinorChanges/Example1.cs
@@ -9,11 +9,17 @@ namespace Challenges.CSharpIND3.CSharp4.MinorChanges
             Console.WriteLine("x={0}, y={1}, z={2},", x, y, z);
         }
 
+        public void Named(string s1, string s2)
+        {
+            Console.WriteLine(s1 + s2);
+        }
+
         public void Go()
         {
             Optional(1, 2, 3);
             Optional(1, 2);
             Optional(1);
+            Named(s2: "second", s1: "first");
         }
     }
 }
