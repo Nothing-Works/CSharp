8a9570562d650bb7422708f0f01856076683dddf
AndySong <andylauszp@gmail.com>
2019-07-31 15:44:56 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp4/Dynamic4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp4
+{
+    public class Dynamic4
+    {
+        public void Go()
+        {
+            dynamic text = "hello world";
+            string world = text.Substring(6);
+            Console.WriteLine(world);
+
+            string broken = text.SUBSTR(6);
+            Console.WriteLine(broken);
+
+            Add1("text");
+            Add1(10);
+            Add1(TimeSpan.FromMinutes(45));
+        }
+
+
+        private void Add1(dynamic d)
+        {
+            Console.WriteLine(d + d);
+        }
+    }
+}
