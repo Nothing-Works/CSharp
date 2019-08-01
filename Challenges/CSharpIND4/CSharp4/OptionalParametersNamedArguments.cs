1ef0e3806891dc7e511cce7c5507071c9619c8db
AndySong <andylauszp@gmail.com>
2019-08-01 15:39:17 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp4/OptionalParametersNamedArguments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp4
+{
+    public class OptionalParametersNamedArguments
+    {
+        public void Go(int x, int y = 5, int z = 10)
+        {
+            Console.WriteLine("x={0}; y={1}; z={2}", x, y, z);
+        }
+    }
+}
