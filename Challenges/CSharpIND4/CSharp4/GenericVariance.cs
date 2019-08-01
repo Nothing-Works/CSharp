17f0d6556f1a3f67cfad75615e1b5cec5db77827
AndySong <andylauszp@gmail.com>
2019-08-01 16:57:23 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp4/GenericVariance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challenges.CSharpIND4.CSharp4
+{
+    public class GenericVariance
+    {
+        public void Go()
+        {
+            //Variance
+
+            //Covariance
+            IEnumerable<string> strings = new List<string> { "a", "b", "c" };
+            IEnumerable<object> objects = strings;
+
+
+            //Contravariance
+            Action<object> objectAction = c => Console.WriteLine(c);
+            Action<string> stringAction = objectAction;
+
+            stringAction("Andy");
+
+            //IEnumerable values as only output,Action values as only input,IList values as both input and output. 
+
+            //Invariance
+            //will not work. as IList can do both input and output, whereas IEnumerable can only do output.
+            //IList<string> lists = new List<string> { "a", "b", "c" };
+            //IList<object> list1 = lists;
+        }
+    }
+}
