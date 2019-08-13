583b8d7047b5b8f73b0f1862b968704c5b9721a7
AndySong <andylauszp@gmail.com>
2019-08-13 15:43:49 +1200

    wip

--- Challenges/CSharpIND4/CSharp5/Example.cs
+++ Challenges/CSharpIND4/CSharp5/Example.cs
@@ -22,6 +22,54 @@ namespace Challenges.CSharpIND4.CSharp5
             return result;
         }
 
+        public Task<int> LengthAnonymous(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("you passed null");
+            }
+
+            Func<string, Task<int>> met = async c =>
+            {
+                await Task.Delay(3000);
+                return c.Length;
+            };
+
+            return met(s);
+        }
+
+        public Task<int> LengthLocal(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("you passed null");
+            }
+
+            return Ipl(s);
+
+            async Task<int> Ipl(string t)
+            {
+                await Task.Delay(3000);
+                return t.Length;
+            }
+        }
+
+        public Task<int> LengthAsync(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("you passed null");
+            }
+
+            return LengthAsyncIpl(s);
+        }
+
+        private async Task<int> LengthAsyncIpl(string s)
+        {
+            await Task.Delay(3000);
+            return s.Length;
+        }
+
         private Task<int> Test()
         {
             return Task.Run(() => 1);
