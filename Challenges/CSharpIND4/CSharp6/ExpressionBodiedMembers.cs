c327a257fa08f81aa0773cde5444b232ccf85745
AndySong <andylauszp@gmail.com>
2019-08-26 16:23:40 +1200

    expressionbodied members

---
+++ Challenges/CSharpIND4/CSharp6/ExpressionBodiedMembers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp6
+{
+    public class ExpressionBodiedMembers
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public ExpressionBodiedMembers(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double DistanceFromOrigin
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        //C#6
+        public double DistanceFromOrigin1 => Math.Sqrt(X * X + Y * Y);
+
+        public int AddTwo(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int AddTwo1(int x, int y) => x + y;
+    }
+}
---
+++ Challenges/CSharpIND4/CSharp7/ExBodiedMembers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp7
+{
+    public class ExBodiedMembers
+    {
+        static ExBodiedMembers() => Console.WriteLine("Static constructor called");
+        ~ExBodiedMembers() => Console.WriteLine("Finalizer called");
+        private string name;
+        public ExBodiedMembers(string name) => this.name = name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+}
