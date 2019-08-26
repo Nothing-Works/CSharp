f053ed8cbfb44d3a92f378d17e6f0b4a2067d6fc
AndySong <andylauszp@gmail.com>
2019-08-26 16:44:56 +1200

    wip

---
+++ Challenges/CSharpIND4/CSharp6/RecapStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Challenges.CSharpIND4.CSharp6
+{
+    public class RecapStrings
+    {
+        public void Hello()
+        {
+            Console.WriteLine("what's is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+            var name1 = string.Format("Hello, {0}!", name);
+        }
+    }
+}
