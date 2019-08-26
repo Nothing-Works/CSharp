4f9c445684687f00271e01b24e36cb0b40bab1ef
AndySong <andylauszp@gmail.com>
2019-08-26 15:20:05 +1200

    auto properties

---
+++ Challenges/CSharpIND4/CSharp6/InitializingProperties.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Challenges.CSharpIND4.CSharp6
+{
+    public class InitializingProperties
+    {
+        //C#2
+        private List<int> nums = new List<int>();
+
+        public List<int> Nums
+        {
+            get { return nums; }
+            set { nums = value; }
+        }
+
+        //C#3
+        public List<int> nums3 { get; set; }
+
+        public InitializingProperties()
+        {
+            nums3 = new List<int>();
+        }
+
+        //C#6
+        public List<int> num6 { get; set; } = new List<int>();
+
+        //ReadOnly one
+        public List<int> num7 { get; } = new List<int>();
+    }
+}
---
+++ Challenges/CSharpIND4/CSharp6/Point.cs
@@ -0,0 +1,24 @@
+﻿namespace Challenges.CSharpIND4.CSharp6
+{
+    public struct Point
+    {
+        //        Before C#6
+        //        public double X { get; private set; }
+        //        public double Y { get; private set; }
+        //
+        //        public Point(double x, double y) : this()
+        //        {
+        //            X = x;
+        //            Y = y;
+        //        }
+
+        public int X1 { get; }
+        public int Y1 { get; }
+
+        public Point(int x, int y)
+        {
+            X1 = x;
+            Y1 = y;
+        }
+    }
+}
