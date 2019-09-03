aebfbc6b7f93ebe38dafa352dfe70d3606203693
AndySong <andylauszp@gmail.com>
2019-09-03 16:14:54 +1200

    wip

--- Challenges/CSharpIND4/CSharp7/PatternMatching.cs
+++ Challenges/CSharpIND4/CSharp7/PatternMatching.cs
@@ -75,5 +75,18 @@ namespace Challenges.CSharpIND4.CSharp7
             throw new ArgumentException(
                 $"Shape type {shape.GetType()} perimeter unknown", nameof(shape));
         }
+
+        public int Fib(int n)
+        {
+            switch (n)
+            {
+                case 0: return 0;
+                case 1: return 1;
+                case var _ when n > 1: return Fib(n - 2) + Fib(n - 1);
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        nameof(n), "Input must be non-negative");
+            }
+        }
     }
 }
